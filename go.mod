547ba73cd66a4176e8d13864620a926921269250
devher0 <nichita.perestoronin@ops8.io>
2024-07-24 18:09:28 +0300

    feat: add big barks

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/ops8-io/puppy
 
 go 1.21.1
 
+require github.com/ops8-io/dog v0.0.0-20240724150505-a6fca08bc57b // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/ops8-io/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
